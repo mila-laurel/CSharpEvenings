ea10a15110ec53319666b70548d914355d45a743
mila-laurel <62745003+mila-laurel@users.noreply.github.com>
2020-09-02 22:17:26 +0300

    changed to generic

--- DataStructures/LinkedList/LinkedList.cs
+++ DataStructures/LinkedList/LinkedList.cs
@@ -1,11 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
 
 namespace DataStructures.LinkedList
 {
-    class LinkedList<T> : IEnumerable
+    class LinkedList<T> : IEnumerable<T>
     {
         public ListNode<T> Head { get; private set; }
-
+        
         public void Add(T element)
         {
             ListNode<T> node = new ListNode<T>(element, Head);
@@ -20,18 +21,24 @@ namespace DataStructures.LinkedList
                 Head = node;
                 return;
             }
-            
+
             ListNode<T> tailNode = Head;
-            while(tailNode.Next != null)
+            while (tailNode.Next != null)
             {
                 tailNode = tailNode.Next;
             }
             tailNode.Next = node;
         }
 
+        
         public IEnumerator GetEnumerator()
         {
             return (IEnumerator)GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
