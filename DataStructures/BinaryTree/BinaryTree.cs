ecf4cc9a926d8b84e15cc24b628808c5d7dfb335
mila-laurel <62745003+mila-laurel@users.noreply.github.com>
2020-09-16 23:41:06 +0300

    something about binaryTree but nothing substantial

---
+++ DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DataStructures.BinaryTree
+{
+    class BinaryTree<T> : IComparable<T>
+    {
+        public TreeNode<T> Root { get; private set; }
+        public delegate Func<T, T, bool> compare();
+
+        public BinaryTree(T data)
+        {
+            Root = new TreeNode<T>(data);
+            
+        }
+
+        public void Add(T element)
+        {
+            if (Root == null)
+            {
+                Root.Data = element;
+                return;
+            }
+            TreeNode<T> currentNode = Root;
+            Traverse(element, currentNode);
+        }
+
+        public void Traverse(T element, TreeNode<T> node)
+        {
+            if (element.Equals(node.Data))
+                node = node.RightChild;
+            else
+                node = node.LeftChild;
+            Traverse(element, node);
+        }
+
+        public void Remove(T element)
+        {
+            TreeNode<T> rootNode = new TreeNode<T>(element, Root);
+            RemoveValue(rootNode, rootNode.RightChild, element);
+            rootNode = Root;
+        }
+
+        private void RemoveValue(TreeNode<T> parentNode, TreeNode<T> currentNode, T element)
+        {
+            if (currentNode == null)
+                return;
+
+            if (currentNode.Data.Equals(element))
+            {
+                parentNode.RightChild = currentNode.RightChild;
+                return;
+            }
+
+            RemoveValue(currentNode, currentNode.RightChild, element);
+        }
+
+        public int CompareTo([AllowNull] T other)
+        {
+            if (other == null)
+                return 0;
+            return 1;
+        }
+    }
+}
---
+++ DataStructures/BinaryTree/TreeNode.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Principal;
+
+namespace DataStructures.BinaryTree
+{
+    class TreeNode<T> :IComparable<T>
+    {
+        public T Data { get; set; }
+        public TreeNode<T> LeftChild { get; set; }
+        public TreeNode<T> RightChild { get; set; }
+
+        public TreeNode(T data, TreeNode<T> left = null, TreeNode< T > right = null)
+        {
+            Data = data;
+            LeftChild = left;
+            RightChild = right;
+        }
+
+        public int CompareTo([AllowNull] T other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
