82cce02277838ecba47e61414622afb4bb1b68b0
mila-laurel <62745003+mila-laurel@users.noreply.github.com>
2020-09-28 22:02:34 +0300

    DFS should be done and transfered to BinaryTree class along with BFS. Delegate should be passed as parameter to set what shoulb be given as a result

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -11,6 +11,38 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            LinkedListMain();
+            BinaryTreeMain();
+        }
+
+        static void LinkedListMain()
+        {
+            var list = new DataStructures.LinkedList.LinkedList<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+
+            list.AddTail(12);
+
+            var head = list.Head;
+            var nextItem = head.Next;
+            var oldNext = nextItem.Next;
+            nextItem.Next = new ListNode<int>(555, oldNext);
+
+            Print(list);
+
+            Console.WriteLine(list[1]);
+            list[4] = 44;
+            Print(list);
+            list.Remove(5);
+            Print(list);
+            list.Add(5);
+            list.Remove(6);
+            Print(list);
+        }
+
+        static void BinaryTreeMain()
+        {
             var tree = new DataStructures.BinaryTree.BinaryTree<int>(15);
             tree.Add(1);
             tree.Add(3);
@@ -22,25 +54,6 @@ namespace DataStructures
             tree.Add(16);
 
             BFS(tree.Root);
-
-            //list.AddTail(12); 
-
-            //var head = list.Head;
-            //var nextItem = head.Next;
-            //var oldNext = nextItem.Next;
-            //nextItem.Next = new ListNode<int>(555, oldNext);
-
-            //Print(list);
-
-            //Console.WriteLine(list[1]);
-            //list[4] = 44;
-            //Print(list);
-            //list.Remove(5);
-            //Print(list);
-            //var list = new DataStructures.LinkedList.LinkedList<int>();
-            //list.Add(5);
-            //list.Remove(6);
-            //Print(list);
         }
 
         static void Print<T>(IEnumerable<T> collection)
@@ -63,6 +76,22 @@ namespace DataStructures
                     queue.Enqueue(currentNode.LeftChild);
                 if (currentNode.RightChild != null)
                     queue.Enqueue(currentNode.RightChild);
+                Console.WriteLine(currentNode.Data);
+            }
+        }
+
+        static void DFS(TreeNode<int> node)
+        {
+            Stack<TreeNode<int>> stack = new Stack<TreeNode<int>>();
+            stack.Push(node);
+
+            while (stack.Count > 0)
+            {
+                TreeNode<int> currentNode = stack.Pop();
+                if (currentNode.LeftChild != null)
+                    stack.Push(currentNode.LeftChild);
+                if (currentNode.RightChild != null)
+                    stack.Push(currentNode.RightChild);
                 Console.WriteLine(currentNode.Data);
             }
         }
